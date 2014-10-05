2dc172169d7d05c890250338345fd5f0923dbb6d
Eddy Shure <mono@tucana.uberspace.de>
2014-10-05 15:22:08 +0200

    Update this script.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-blitz
-=====
+# blitz
 
-Generating true random numbers based on lightning strokes.
+blitz allows you to get lightning events from blitzortung.org
+
+## Usage
+```bash
+./blitz [geo_code]
+```
+
+## Geocodes
+
+### Europe
+
+|10|Europe               |
+|--|---------------------|
+|11|Scandinavia          |
+|--|---------------------|
+|12|United Kingdom       |
+|--|---------------------|
+|13|Southwest Europe     |
+|--|---------------------|
+|14|Southeast Europe     |
+|--|---------------------|
+|15|Eastern Europe       |
+|--|---------------------|
+|16|Central Europe       |
+|--|---------------------|
+
+
--- blitz_rand.rb
+++ blitz_rand.rb
@@ -7,19 +7,38 @@ require 'eventmachine'
 maps = JSON.parse(File.read("maps.json"))
 map = maps["maps"][ARGV.first.to_str]
 
+@i = 0
+@time = Array.new(4)
+
+def gen_rand(click)
+  if @i < 4
+    @time[@i] = click
+    @i += 1
+  elsif @i == 4
+    length1 = @time[1] - @time[0]
+    length2 = @time[3] - @time[2]
+    if length2 > length1
+      puts "1"
+    elsif length2 < length1
+      puts "0"
+    else; end
+    @i = 0 
+  end
+end
+
 EM.run {
   ws = Faye::WebSocket::Client.new("ws://websocketserver.blitzortung.org:801" + "#{(rand() * 10).to_i}")
 
   ws.on :open do |event|
+    puts "Starting to generate random numbers from lighting strokes."
     ws.send "{\"region\":#{map["region"]},\"west\":#{map["west"]},\"east\":#{map["east"]},\"north\":#{map["north"]},\"south\":#{map["south"]}}" 
   end
 
   ws.on :message do |event|
-    p [:message, event.data]
+    gen_rand(Time.now.to_i)
   end
 
   ws.on :close do |event|
-    p [:close, event.code, event.reason]
     ws = nil
   end
 }
